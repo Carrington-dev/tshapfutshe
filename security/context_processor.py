57c328db277c639b749b8ec4e4a38f405005aa36
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-28 14:31:47 +0200

    Added Context processor

---
+++ security/context_processor.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+def show_data(request):
+    """
+    Context processor to add user data to the context.
+    """
+    company = "Top Tutors"
+    return {
+        "company": company,
+        "company_title": company,
+        "year": 2025,
+        "phone": '+27 (0) 67 735 2242',
+        "title": 'South Africa’s Leading Tutoring Company',
+        "description": 'Top Tutors is a leading tutoring company in South Africa, providing personalized tutoring services to students of all ages. Our experienced tutors are dedicated to helping students achieve their academic goals and reach their full potential.',
+        "email": 'hello@toptutors.co.za',
+        "shop_email": 'uniforms@toptutors.co.za',
+        "address": "410 Francis Baard Street</p><p>Pretoria Central, 0001</p><p>South Africa",
+        "copyright_notice": f"© {datetime.now().year} { company }. All rights reserved.",
+        "copyright": f"""© <span>{datetime.now().year}</span><strong class="px-1 sitename">{ company }.</strong> <span>All Rights Reserved.</span>""",
+    }
--- tshaweb/settings.py
+++ tshaweb/settings.py
@@ -49,6 +49,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Custom context processor for user data
+                'security.context_processor.show_data',
+                'django.template.context_processors.debug',
+                # 'django.template.context_processors.i18n',
+                # 'django.template.context_processors.media',
+                # 'django.template.context_processors.static',
             ],
         },
     },
