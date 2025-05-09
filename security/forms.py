aa3fc82bcab254824fb441c53727952747631c51
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-05-09 07:50:52 +0200

    Translated Order Form

--- security/forms.py
+++ security/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from security.models import Order
+from django.utils.translation import gettext_lazy as _
 
 class OrderForm(ModelForm):
     class Meta:
@@ -20,6 +21,21 @@ class OrderForm(ModelForm):
             # 'status',
             'short_description',
         ]
+        labels = {
+            'first_name': _("First Name"),
+            'last_name': _("Last Name"),
+            'email': _("Email Address"),
+            'country': _("Country"),
+            'phone': _("Phone Number"),
+            'order_total': _("Order Total"),
+            'payment_method': _("Payment Method"),
+            'state': _("State"),
+            'city': _("City"),
+            'street_name': _("Street Name"),
+            'zip_code': _("Zip Code"),
+            'short_description': _("Short Description"),
+            # 'status': _("Status"),
+        }
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
